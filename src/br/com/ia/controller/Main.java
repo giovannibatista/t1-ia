b561052af8f36989e66b04a047261d73fb1a96ce
lucasmedi <lucas.cunha2104@gmail.com>
2014-09-10 15:55:04 -0300

    Adaptação do Main.java
    Adaptação com o novo formato dos blocos (só para compilar).

--- src/br/com/ia/controller/Main.java
+++ src/br/com/ia/controller/Main.java
@@ -1,11 +1,12 @@
-package br.com.ia.controller; 
+package br.com.ia.controller;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.ia.agents.Agent;
 import br.com.ia.agents.Collector;
-import br.com.ia.agents.Recharge;
+import br.com.ia.agents.Recharger;
 import br.com.ia.agents.TrashCan;
 import br.com.ia.utils.TrashCanType;
 import br.com.ia.utils.TrashCanTypeGenerator;
@@ -23,19 +24,17 @@ public class Main {
 
 	private Collector collector;
 
-
-	@PostConstruct 
-	public void init(){
+	@PostConstruct
+	public void init() {
 		System.out.println(" Bean executado! ");
 
-		//Valores iniciais
+		// Valores iniciais
 		columns = 10;
 		amountTrashCans = 4;
 		amountRecharges = 1;
 	}
 
-
-	public void run(){
+	public void run() {
 		rows = columns;
 		agents = new Agent[rows][columns];
 
@@ -53,84 +52,69 @@ public class Main {
 
 	}
 
-	public void next(){
+	public void next() {
 
-		if(!hasElement(collector.getAxisX(),collector.getAxisY()+1)){
+		/*if (!hasElement(collector.getAxisX(), collector.getAxisY() + 1)) {
 			agents[collector.getAxisX()][collector.getAxisY()] = null;
-			collector.setAxisY(collector.getAxisY()+1);
+			collector.setAxisY(collector.getAxisY() + 1);
 			agents[collector.getAxisX()][collector.getAxisY()] = collector;
-		}
+		}*/
 
 	}
 
-	public void insertTrashCan(){
-		Integer x = (int)(Math.random() * 10);
-		Integer y = (int)(Math.random() * 10);
+	public void insertTrashCan() {
+		Integer x = (int) (Math.random() * 10);
+		Integer y = (int) (Math.random() * 10);
 
-		if(hasElement(x,y))
+		if (hasElement(x, y))
 			insertTrashCan();
 
 		TrashCanType trashCanType = TrashCanTypeGenerator.next();
 		String img = TrashCanTypeGenerator.getTrashCanTypeIcon(trashCanType);
-		TrashCan trashCan = new TrashCan("Lixeira",img, false, 10, TrashCanTypeGenerator.next(), x, y);
+		TrashCan trashCan = new TrashCan("Lixeira", img, 10, TrashCanTypeGenerator.next(), x, y);
 
 		agents[x][y] = trashCan;
 
-		collector.getTrasCans().add(trashCan);
+		collector.addTrashCan(x, y);
 
 	}
 
-	public void insertRecharges(){
-		Integer x = (int)(Math.random() * 10);
-		Integer y = (int)(Math.random() * 10);
+	public void insertRecharges() {
+		Integer x = (int) (Math.random() * 10);
+		Integer y = (int) (Math.random() * 10);
 
-		if(hasElement(x,y))
+		if (hasElement(x, y))
 			insertRecharges();
 
-		Recharge recharge = new Recharge("Recarga", x, y);
+		Recharger recharge = new Recharger("Recarga", x, y);
 
 		agents[x][y] = recharge;
 
-		collector.getRecharges().add(recharge);
+		collector.addRecharger(x, y);
 
 	}
 
-
-	//Validar posicao dos agentes
-	public boolean hasElement(Integer x, Integer y){
+	// Validar posicao dos agentes
+	public boolean hasElement(Integer x, Integer y) {
 		System.out.println("----------------->>" + x + ", " + y);
 
-		if(agents[x][y] != null){
+		if (agents[x][y] != null) {
 			return true;
 		}
 
-		/*if(x > 0 && x < rows){
-			if(agents[x+1][y] != null)
-				return true;
-			else if(agents[x-1][y] != null)
-				return true;
-		}
-		if(y > 0 && y < columns){
-			if(agents[x][y+1] != null)
-				return true;
-			else if(agents[x][y-1] != null)
-				return true;
-		}
-		if(x > 0 && x < rows && y > 0 && y < columns){
-			if(agents[x+1][y+1] != null)
-				return true;
-			else if(agents[x-1][y-1] != null)
-				return true;
-			else if(agents[x+1][y-1] != null)
-				return true;
-			else if(agents[x-1][y+1] != null)
-				return true;
-		}*/
+		/*
+		 * if(x > 0 && x < rows){ if(agents[x+1][y] != null) return true; else
+		 * if(agents[x-1][y] != null) return true; } if(y > 0 && y < columns){
+		 * if(agents[x][y+1] != null) return true; else if(agents[x][y-1] !=
+		 * null) return true; } if(x > 0 && x < rows && y > 0 && y < columns){
+		 * if(agents[x+1][y+1] != null) return true; else if(agents[x-1][y-1] !=
+		 * null) return true; else if(agents[x+1][y-1] != null) return true;
+		 * else if(agents[x-1][y+1] != null) return true; }
+		 */
 		return false;
 	}
 
-
-	public String getMessage(){ 
+	public String getMessage() {
 		return "Hello World JSF!";
 	}
 
@@ -181,8 +165,5 @@ public class Main {
 	public void setCollector(Collector collector) {
 		this.collector = collector;
 	}
-
-
-
 
 }
